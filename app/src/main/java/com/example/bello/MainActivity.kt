c607085cca75c4538dd1353329e3579d001c5060
AdityaRJha <adityadevop@gmail.com>
2021-08-27 14:26:05 +0530

    Action Bar removed.

--- app/src/main/java/com/example/bello/MainActivity.kt
+++ app/src/main/java/com/example/bello/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //removing the Action Bar
+        supportActionBar?.hide()
     }
 }
